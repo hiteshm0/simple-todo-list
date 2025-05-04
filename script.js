c01eab3e1c56bbf10d839ac44471c26ea4dc39ea
hiteshm0 <hiteshm0399@gmail.com>
2025-05-04 14:53:54 +0530

    final

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let toDoList = JSON.parse(localStorage.getItem('toDoList')) || [];
 let currentView = 'inbox'; // Default view is 'inbox' (all tasks)
+let editingIndex = -1; // Track which task is being edited
 
 document.getElementById('add').onclick = function() {
     addToDo();
@@ -42,14 +43,26 @@ document.getElementById('profile').addEventListener('click', function() {
 function addToDo() {
     let task = document.getElementById('input').value;
     let date = document.getElementById('date').value;
-    if (task.trim() !== '') {
-        toDoList.push({ task, date });
-        localStorage.setItem('toDoList', JSON.stringify(toDoList));
-        console.log(toDoList);
-        displayList();
-        document.getElementById('input').value = '';
-        document.getElementById('date').value = '';
+    
+    if (editingIndex >= 0) {
+        // Update existing task
+        if (task.trim() !== '') {
+            toDoList[editingIndex] = { task, date };
+            localStorage.setItem('toDoList', JSON.stringify(toDoList));
+            editingIndex = -1;
+            document.getElementById('add').textContent = 'Add';
+        }
+    } else {
+        // Add new task
+        if (task.trim() !== '') {
+            toDoList.push({ task, date });
+            localStorage.setItem('toDoList', JSON.stringify(toDoList));
+        }
     }
+    
+    displayList();
+    document.getElementById('input').value = '';
+    document.getElementById('date').value = '';
 }
 
 function displayList() {
@@ -68,17 +81,42 @@ function displayList() {
     }
     
     for (let i = 0; i < tasksToDisplay.length; i++) {
+        const taskIndex = toDoList.indexOf(tasksToDisplay[i]);
         document.getElementsByClassName('js-to-do-list-container')[0].innerHTML += 
         `
         <div class="element-container">
-            <div class="to-do-list-element">${i+1}. ${tasksToDisplay[i].task}</div>
+            <div class="to-do-list-element">
+                <input type="checkbox" class="task-checkbox" onclick="completeTask(${taskIndex})">
+                <span>${tasksToDisplay[i].task}</span>
+            </div>
             <div class="to-do-list-date">${tasksToDisplay[i].date}</div>
-            <button class="remove-button" onclick="removeElement(${toDoList.indexOf(tasksToDisplay[i])})">Remove</button>
+            <button class="edit-button" onclick="editTask(${taskIndex})">Edit</button>
         </div>
         `;
     }
 }
 
+function editTask(i) {
+    // Set the form values to the task being edited
+    document.getElementById('input').value = toDoList[i].task;
+    document.getElementById('date').value = toDoList[i].date;
+    document.getElementById('add').textContent = 'Update';
+    
+    // Set the editing index
+    editingIndex = i;
+    
+    // Focus on the input
+    document.getElementById('input').focus();
+}
+
+function completeTask(i) {
+    // Remove the task when checkbox is clicked
+    toDoList.splice(i, 1);
+    localStorage.setItem('toDoList', JSON.stringify(toDoList));
+    displayList();
+}
+
+// Keeping this for compatibility but no longer used in UI
 function removeElement(i) {
     toDoList.splice(i, 1);
     localStorage.setItem('toDoList', JSON.stringify(toDoList));
